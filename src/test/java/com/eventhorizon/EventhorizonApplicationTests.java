aa97b2d6c23dfe8418a3f46fb0669fbfb2168404
Eduber Enrique Santamaria Vertel <160356279+EduverVertel@users.noreply.github.com>
2026-08-20 21:49:42 -0500

    Configuraciones previas al desarrollo de Event Horizon

---
+++ src/test/java/com/eventhorizon/EventhorizonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventhorizon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventhorizonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
